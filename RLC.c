ee83804e92470fbba8fed7f41f3d39bc48e0b7ca
fadhilaramadiastri <60211901+fadhilaramadiastri@users.noreply.github.com>
2020-04-24 11:36:46 +0700

    Update RLC.c

--- RLC.c
+++ RLC.c
@@ -51,20 +51,27 @@ void hitungRLC(double *R, double *L, double *C, double *V, int *t)
     double omega = 1/sqrt(L*C);
     double alpha = R/(2*L);
     double s1, s2, A1, A2;
+    double io = V/R;
     s1= -alpha + sqrt((alpha*alpha)-(omega*omega));
     s2= -alpha - sqrt((alpha*alpha)-(omega*omega));
     vss = V;
     
     if(alpha > omega)
-    {       
+    {   
+        A2 = (((1/C)*io)+vss*s1)/(-s1+s2));
+        A1 = -A2-vss;
         vt = (A1*exp(s1*t))+(A2*exp(s2*t));
     }
     else if(alpha == omega)
     {
+        A1 = -vss;
+        A2 = ((1/C)*io)+2*alpha+vss;
         vt = (A1+(A2*t))*(exp(-alpha*t));
     }
     else
     {
+        A1 = -vss;
+        A2 = ((1/C)*io)/(omega*-alpha);
         vt = ((A1*cos(omega*t))+(A2*sin(omega*t)))*(exp(-alpha*t));
     }
 
